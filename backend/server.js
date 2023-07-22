b8d4a70a880b664c5556732301aca9b274122861
Voislav Ivanovski <voislav21@gmail.com>
2023-07-22 12:14:08 +0200

    Backend implementation

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express()
+
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+//MIDDLEWARES
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+
+app.listen(8080, () => {
+    console.log("Api Working!")
+});
