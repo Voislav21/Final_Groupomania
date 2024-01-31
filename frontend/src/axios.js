b9a1aeef789eeeb076e8c2e689cdf2fbc474981c
Voislav Ivanovski <voislav21@gmail.com>
2024-01-31 17:34:47 +0100

    Vite config to stringify JSON

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = `${import.meta.env.VITE_API_BASE_URL}/api` || "https://api.groupomania-voislav.com";
+const baseURL = `${import.meta.env.VITE_API_BASE_URL}/api` || "https://api.groupomania-voislav.com/api";
 
 export const makeRequest = axios.create({
   baseURL,
